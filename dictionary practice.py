d7f0363e4c8de90389aae7d838f297d009c71289
anoushka1111 <saha.anoushka@gmail.com>
2024-05-08 12:11:31 -0400

    practicing dictionaries

---
+++ dictionary practice.py
@@ -0,0 +1,25 @@
+user_input = input(print("Who's grades are you looking for?\n"))
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+for key in student_scores:
+    if key == user_input:
+        print("The student you are looking for is " + user_input + " and their grade is " + str(student_scores[key]))
+        if student_scores[key] >= 91:
+            print("This grade is outstanding")
+        elif student_scores[key] >= 81 and student_scores[key] <= 90:
+            print("This grade exceeds expectations")
+        elif student_scores[key] >= 71 and student_scores[key] <= 80:
+            print("This grade is acceptable")
+        else:
+            print("This grade is a fail")
+    else:
+        break
+        
+print("The student you are looking for is not in our system")
